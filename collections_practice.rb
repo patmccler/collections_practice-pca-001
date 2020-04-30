1cff20de5d41175869157478d30c11172a6b3e67
Pat McClernan <patmccler@gmail.com>
2020-04-30 12:00:19 -0400

    build methods to pass tests

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,58 @@
 
+
+def sort_array_asc array
+  array.sort
+
+end
+
+def sort_array_desc array
+  array.sort { |a,b| -1 * (a <=> b)}
+
+end
+
+
+def sort_array_char_count array
+  array.sort {|a,b| a.length <=> b.length}
+
+end
+
+# swap second and third elements
+def swap_elements array
+  temp = array[1]
+  array[1] = array[2]
+  array[2] = temp
+
+  array
+end
+
+def reverse_array array
+  new_array = []
+  array.each do|elem|
+     new_array.unshift elem
+  end
+  new_array
+end
+
+# change third char of each element to $
+def kesha_maker array
+  array.map {|elem| elem[0,2] + "$" + elem[3,elem.length]}
+  
+end
+
+#find all words that begin with a
+def find_a array
+  array.select {|elem| elem[0] == "a"}
+
+end
+
+# sum all the numbers
+def sum_array array
+  sum = 0
+  array.each {|elem| sum += elem}
+  sum
+end
+
+#add s to all but second element
+def add_s array
+  array.map.with_index {|elem, i| i != 1? elem + "s" : elem}
+end
